229fc7a6d8ae26e6b64feebda9a55fa55bf7e3f7
Can Elkatmış <canelkatmis@gmail.com>
2019-12-26 15:09:38 +0300

    Create sg.tf

---
+++ terraform/sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "harbor_instance_sg" {
+  name   = "harbor_instance_sg"
+  vpc_id = "${aws_vpc.VPC.id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name        = "harbor_instance_sg.${var.environment_tag}"
+    Environment = "${var.environment_tag}"
+  }
+}
