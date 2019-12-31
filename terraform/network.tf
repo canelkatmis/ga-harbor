2376d8673ac00bc1aeea94323eac506b3f6bffc4
Can Elkatmış <canelkatmis@gmail.com>
2019-12-31 13:57:32 +0300

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -53,8 +53,8 @@ resource "aws_route_table_association" "PubSubnetRT" {
 
 resource "aws_network_acl" "ACL" {
   vpc_id = aws_vpc.VPC.id
-  subnet_ids = concat(aws_subnet.PublicSubnet.*.id,
-  aws_subnet.PrivateSubnet.*.id)
+  subnet_ids = "${concat(aws_subnet.PublicSubnet.*.id,
+  aws_subnet.PrivateSubnet.*.id)}"
   egress {
     protocol   = -1
     rule_no    = 100
